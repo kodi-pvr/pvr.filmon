b35c779fd521e2f123a4d69e74d6ecb39d2e9af5
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2018-06-08 21:25:29 +0200

    PVR API 5.10.0

--- src/client.cpp
+++ src/client.cpp
@@ -493,4 +493,8 @@ PVR_ERROR GetEPGTagStreamProperties(const EPG_TAG*, PVR_NAMED_VALUE*, unsigned i
 PVR_ERROR GetEPGTagEdl(const EPG_TAG* epgTag, PVR_EDL_ENTRY edl[], int *size) {
   return PVR_ERROR_NOT_IMPLEMENTED;
 }
+PVR_ERROR GetStreamReadChunkSize(int* chunksize) {
+  return PVR_ERROR_NOT_IMPLEMENTED;
+}
+
 }
