ece70b8afe2dfd075eaed88440c3c3fdffc577d5
Alwin Esch <alwin.esch@web.de>
2020-03-24 13:48:37 +0100

    code style cleanups

--- src/md5.h
+++ src/md5.h
@@ -21,35 +21,37 @@
 #ifndef _MD5_H_
 #define _MD5_H_
 
-#include <string>
 #include <stdint.h>
+#include <string>
 
-struct MD5Context {
-	uint32_t buf[4];
-	uint32_t bytes[2];
-	uint32_t in[16];
+struct MD5Context
+{
+  uint32_t buf[4];
+  uint32_t bytes[2];
+  uint32_t in[16];
 };
 
 namespace PVRXBMC
 {
-  class XBMC_MD5
-  {
-  public:
-    XBMC_MD5(void);
-    ~XBMC_MD5(void);
-    void append(const void *inBuf, size_t inLen);
-    void append(const std::string& str);
-    void getDigest(unsigned char digest[16]);
-    std::string getDigest();
+class XBMC_MD5
+{
+public:
+  XBMC_MD5(void);
+  ~XBMC_MD5(void);
+  void append(const void* inBuf, size_t inLen);
+  void append(const std::string& str);
+  void getDigest(unsigned char digest[16]);
+  std::string getDigest();
 
-    /*! \brief Get the MD5 digest of the given text
-     \param text text to compute the MD5 for
-     \return MD5 digest
-     */
-    static std::string GetMD5(const std::string &text);
+  /*! \brief Get the MD5 digest of the given text
+      \param text text to compute the MD5 for
+      \return MD5 digest
+      */
+  static std::string GetMD5(const std::string& text);
+
 private:
-    MD5Context m_ctx;
-  };
-}
+  MD5Context m_ctx;
+};
+} // namespace PVRXBMC
 
 #endif
